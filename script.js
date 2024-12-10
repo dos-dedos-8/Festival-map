8492b6b6dc5d9f5e2bac22097b720c2a9c96194a
dos-dedos-8 <lukewestmoreland@icloud.com>
2024-12-11 10:13:51 +1300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-let map;
-
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -36.8485, lng: 174.7633 }, // Replace with festival coordinates
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -36.860260, lng: 174.635300 }, // coordinates trust aena (Auckland)
     zoom: 15,
-    mapTypeId: "roadmap",
   });
 }
