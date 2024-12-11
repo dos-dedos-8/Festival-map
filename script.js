6b38c72f0b106ca36349c7289566d4be63c8c7ca
dos-dedos-8 <lukewestmoreland@icloud.com>
2024-12-12 10:10:40 +1300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ function loadGoogleMapsAPI(callback) {
   const script = document.createElement("script");
   script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyDJyXNG2JeQ9V6mhgZwO12pwryeEpZ7GjU&callback=${callback}&libraries=marker&v=weekly`;
   script.async = true;
-  script.defer = true;
   document.head.appendChild(script);
 }
 
@@ -12,27 +11,13 @@ async function initMap() {
   try {
     // Load required libraries
     const { Map } = await google.maps.importLibrary("maps");
-    const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+    const { Marker } = await google.maps.importLibrary("marker");
 
-    // Initialize the map
-    const map = new Map(document.getElementById("map"), {
-      center: { lat: -36.860260, lng: 174.635300 },
-      zoom: 15,
-    });
-
-    // Add an advanced marker
-    new AdvancedMarkerElement({
-      map: map,
-      position: { lat: -36.860260, lng: 174.635300 },
-      title: "Trust Arena",
-    });
-  } catch (error) {
-    console.error("Map or Marker Initialization Error:", error);
-  }
-}
-
-// Attach initMap to the global window object
-window.initMap = initMap;
-
-// Load the Google Maps API
-loadGoogleMapsAPI("initMap");
+    // Map styles
+    const mapStyles = [
+      {
+        featureType: 'all',
+        elementType: 'labels',
+        stylers: [
+          { color: '#ffffff
+            
